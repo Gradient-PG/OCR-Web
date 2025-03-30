09269c9e06f57052e8fb18546bdd2bbae3d4e236
KKucht <krzysztof.k2001.01@wp.pl>
2025-03-30 13:09:29 +0200

    Remove unnecessary code from Task logic

--- donut_service/app/entity/task.py
+++ donut_service/app/entity/task.py
@@ -3,45 +3,10 @@ from enum import Enum
 
 DATEFORMAT = '%Y-%m-%dT%H:%M:%S'
 
-
-class TaskMode(str, Enum):
-    """Enumeration of available tasks"""
-    ZERO_SHOT = 0
-    FEW_SHOT = 1
-
-
-class Language(str, Enum):
-    """Enumeration of available languages"""
-    PL = 'pl'
-    EN = 'en'
-
-
-class Framework(str, Enum):
-    """Enumeration of available frameworks"""
-
-    STORMTROOPER = 'stormtrooper'
-    BULLET = 'bullet'
-    TARS = 'tars'
-    BIELIK_API = 'bielik_api'
-
-
-def map_to_enum(language: str, framework: str):
-    """Convert string representations of specified language and framework to enums defined within this module.
-
-    This function also provides input validation.
-    """
-    try:
-        language_enum = Language(language)
-        framework_enum = Framework(framework)
-
-        if framework_enum == Framework.BIELIK_API and language_enum != Language.PL:
-            raise ValueError('Bielik supports only PL language')
-
-        return language_enum, framework_enum
-
-    except ValueError as e:
-        raise ValueError(f'Wrong input: {e}') from e
-
+class TaskType(str, Enum):
+    AUGMENTATION = 'augmentation'
+    TRAINING = 'training'
+    PREDICTION = 'prediction'
 
 class TaskStatus(str, Enum):
     """Task status enum
@@ -52,8 +17,6 @@ class TaskStatus(str, Enum):
     IN_PROGRESS = 'in_progress'
     COMPLETED = 'completed'
 
-
-# TODO Change ENUM class to class with real result from model
 class TaskResult(str, Enum):
     """Binary task result enum
 
@@ -76,9 +39,7 @@ class TaskEntity:
     id: str | None
     start_time: str
     end_time: str | None = None
-    mode: TaskMode | None = None
-    framework: Framework | None = None
+    task_type: TaskType | None = None
     args: str | None = None  # JSON serialized dict
-    language: Language | None = None
     status: TaskStatus | None = None
     result: TaskResult | None = None
