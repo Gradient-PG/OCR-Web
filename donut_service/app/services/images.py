9a859a0c256db2a2bc6811514b4a4258e3cf14a8
KKucht <krzysztof.k2001.01@wp.pl>
2025-03-30 13:05:36 +0200

    Add service for images
     - now you can stre images in mongodb
     - upload image
     - download iamges as zip file

--- donut_service/app/services/images.py
+++ donut_service/app/services/images.py
@@ -1,13 +1,23 @@
 from typing import List
 from fastapi import UploadFile
+from ..repository.mongodb.images import ImageRepository
+from ..entity.image import ImageEntity
 
 class ImagesService:
 
-    def save_images(self, images: List[UploadFile]):
-        saved_img_id = []
-        for image in images:
-            with open(f"app/images/{image.filename}", "wb") as f:
-                f.write(image.file.read())
-            saved_img_id.append(image.filename)
-        # return {"message": "Images saved successfully"}
-        return saved_img_id
+    def __init__(self, image_repository: ImageRepository):
+        self.image_repository = image_repository
+
+    def save_images(self, input_images: List[UploadFile]):
+        images = [
+            ImageEntity(
+                image_name=image.filename,
+                image_data=image.file.read()
+            )
+            for image in input_images
+        ]
+
+        return self.image_repository.save_images(images)
+
+    def get_all_images(self):
+        return self.image_repository.get_all_images()
